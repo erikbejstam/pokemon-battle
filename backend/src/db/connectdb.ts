4e9c82c358bbc380615982567b31ca663653ba34
erikbejstam <erikbejstam@hotmail.com>
2025-07-25 13:46:15 +0200

    Containerize

--- backend/src/db/connectdb.ts
+++ backend/src/db/connectdb.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectToDB() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/pokemonDB");
+    await mongoose.connect("mongodb://mongodb:27017/pokemonDB");
     console.log("DB connected");
   } catch (err) {
     console.error("DB connection error:", err);
